43dddeb989fc545ca4f25e690078d83552e51f15
Ahmad Almabhoh <az540546@gmail.com>
2022-03-03 04:22:23 +0200

    first commit

---
+++ resources/views/ecommerce/temp.blade.php
@@ -0,0 +1,19 @@
+@extends('ecommerce.index')
+
+@section('page-title', 'your title')
+
+@section('content')
+
+@endsection
+
+@section('aside')
+
+@endsection
+
+@section('styles')
+
+@endsection
+
+@section('scripts')
+
+@endsection
